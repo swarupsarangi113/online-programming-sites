1898b72ed5b46b2aface6d23123a3a33662032e3
Swarup Sarangi <swarupsarangi113@gmail.com>
2020-09-12 10:20:44 +0530

    Create the-pads.sql

---
+++ HackerRank-Solutions/SQL/Advanced Select/the-pads.sql
@@ -0,0 +1,12 @@
+-- https://www.hackerrank.com/challenges/the-pads/problem?isFullScreen=true
+
+SELECT CONCAT(NAME,"(",SUBSTR(OCCUPATION,1,1),")")
+FROM OCCUPATIONS
+ORDER BY NAME;
+
+SELECT CONCAT(
+    'There are a total of ',COUNT(OCCUPATION)," ",LOWER(OCCUPATION),'s.'
+)
+FROM OCCUPATIONS
+GROUP BY OCCUPATION
+ORDER BY COUNT(OCCUPATION);
